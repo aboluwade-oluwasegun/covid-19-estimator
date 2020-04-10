bf76bcf19f830ab613f23fa936e0894b551eafd9
Oluwasegun Aboluwade <aboluwade.oluwasegun@gmail.com>
2020-04-10 10:32:18 +0100

    linting fixed

--- src/estimator.js
+++ src/estimator.js
@@ -55,14 +55,14 @@ const covid19ImpactEstimator = (req, res) => {
   );
 
   // estimated amount of money lost
-  output.impact.dollarsInFlight = (output.impact.infectionsByRequestedTime *
-    req.body.region.avgDailyIncomePopulation *
-    req.body.region.avgDailyIncomeInUSD *
-    req.body.timeToElapse).toFixed(2) * 1;
-  output.severeImpact.dollarsInFlight = (output.severeImpact.infectionsByRequestedTime *
-    req.body.region.avgDailyIncomePopulation *
-    req.body.region.avgDailyIncomeInUSD *
-    req.body.timeToElapse).toFixed(2) * 1;
+  output.impact.dollarsInFlight = (output.impact.infectionsByRequestedTime 
+    * req.body.region.avgDailyIncomePopulation 
+    * req.body.region.avgDailyIncomeInUSD 
+    * req.body.timeToElapse).toFixed(2) * 1;
+  output.severeImpact.dollarsInFlight = (output.severeImpact.infectionsByRequestedTime 
+    * req.body.region.avgDailyIncomePopulation 
+    * req.body.region.avgDailyIncomeInUSD 
+    * req.body.timeToElapse).toFixed(2) * 1;
 
   // xml or json?
   if (req.url === '/api/v1/on-covid-19/xml') {
